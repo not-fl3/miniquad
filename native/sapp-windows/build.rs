936e306bd945bf895462023f45778de6819a5ebc
not-fl3 <not.fl3@gmail.com>
2020-01-06 22:52:29 -0600

    Remove cc-rs build dependency for wasm and linux
    Major refactoring of sokol-app-sys.
    
    Cargo does not support different set of build depdencies for different platforms,
    but especially later for android/ios a lot of platform specific tools will be used.
    
    To workaround this sokol-app-sys was splitted into 3 separate crates:
    
    sapp_wasm: No dependencies, use custom JS instead of sokol's C code
    sapp_linux: Initially c2rust generated, but now rewriten and cleaned up into more
    idiomatic rust code, no C code as well
    sapp_windows: Bindings to a slightly modified sokol_app's C code.

--- native/sapp-windows/build.rs
+++ native/sapp-windows/build.rs
@@ -1,8 +1,8 @@
 extern crate cc;
 
-use std::env;
-
 use cc::{Build, Tool};
+
+use std::env;
 
 fn build_new() -> (Build, Tool) {
     let build = Build::new();
@@ -11,37 +11,15 @@ fn build_new() -> (Build, Tool) {
     (build, tool)
 }
 
-fn select_sokol_gfx_renderer(build: &mut Build) {
-    if cfg!(target_family = "windows") {
-        build.flag("-DSOKOL_GLCORE33");
-    } else if cfg!(target_os = "macos") {
-        build.flag("-DSOKOL_METAL");
-    } else {
-        build.flag("-DSOKOL_GLCORE33");
-    }
-}
-
-fn make_sokol(target: &str) {
+fn build_windows(target: &str) {
     let (mut build, _) = build_new();
 
     let is_debug = env::var("DEBUG").ok().is_some();
-    //
-    // include paths
-    //
-    build
-        .include("external/sokol");
 
-    build
-        .file("src/sokol_app.c");
+    build.include("external/sokol");
+    build.file("src/sokol_app.c");
+    build.flag("-DSOKOL_GLCORE33");
 
-    //
-    // select sokol_gfx renderer
-    //
-    select_sokol_gfx_renderer(&mut build);
-
-    //
-    // silence some warnings
-    //
     build
         .flag_if_supported("-Wno-unused-parameter")
         .flag_if_supported("-Wno-unused-function");
@@ -60,28 +38,21 @@ fn make_sokol(target: &str) {
         println!("cargo:rustc-link-lib=gdi32");
         println!("cargo:rustc-link-lib=ole32");
         println!("cargo:rustc-link-lib=shell32");
-
     }
 
     if is_debug {
-        build
-            .flag("-D_DEBUG")
-            .flag("-DSOKOL_DEBUG");
+        build.flag("-D_DEBUG").flag("-DSOKOL_DEBUG");
     }
 
-    build
-        .compile("sokol-app-sys");
-
-    if target == "x86_64-unknown-linux-gnu" {
-        println!("cargo:rustc-link-lib=dylib=GL");
-        println!("cargo:rustc-link-lib=dylib=X11");
-    }
+    build.compile("sokol-app-sys");
 }
 
 fn main() {
     let target = env::var("TARGET").unwrap_or_else(|e| panic!(e));
 
-    if target != "wasm32-unknown-unknown" {
-        make_sokol(&target);
-    }
+    if target.contains("windows") == false{
+        panic!("Trying to build sapp_linux not for linux!");
+        }
+
+    build_windows(&target);
 }
