2be9215a4d0c68683e9ddaa31b23220719182c06
Vlad <vladzhukovtirko@gmail.com>
2020-01-30 18:36:19 +0300

    rename wasm expose functions

--- native/sapp-wasm/js/gl.js
+++ native/sapp-wasm/js/gl.js
@@ -756,19 +756,19 @@ function init_plugins(plugins) {
         return;
 
     for (var i = 0; i < plugins.length; i++) {
-        plugins[i].init(importObject);
+        plugins[i].register_plugin(importObject);
     }
 }
 
-function init_plugins_wasm(plugins) {
+function expose_wasm(plugins) {
     if (plugins == undefined)
         return;
 
     for (var i = 0; i < plugins.length; i++) {
-        plugins[i].init_wasm(memory, wasm_exports);
+        plugins[i].set_wasm_refs(memory, wasm_exports);
     }
 }
-
+    
 
 function load(wasm_path, plugins) {
     var req = fetch(wasm_path);
@@ -781,7 +781,7 @@ function load(wasm_path, plugins) {
                 memory = obj.instance.exports.memory;
                 wasm_exports = obj.instance.exports;
 
-                init_plugins_wasm(plugins);
+                expose_wasm(plugins);
                 obj.instance.exports.main();
             });
     } else {
@@ -792,7 +792,7 @@ function load(wasm_path, plugins) {
                 memory = obj.instance.exports.memory;
                 wasm_exports = obj.instance.exports;
 
-                init_plugins_wasm(plugins);
+                expose_wasm(plugins);
                 obj.instance.exports.main();
             });
     }
