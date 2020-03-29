c12a306969bf0592e05a3d31eeb6b034fd451bf2
Nokola <nikolami@outlook.com>
2020-03-28 18:37:50 -0700

    Exposed sapp bindings as public to allow custom xplat GL operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[cfg(target_os = "macos")]
-extern crate sapp_darwin as sapp;
+pub extern crate sapp_darwin as sapp;
 #[cfg(not(any(
     target_os = "linux",
     target_os = "macos",
@@ -7,15 +7,15 @@ extern crate sapp_darwin as sapp;
     target_arch = "wasm32",
     windows
 )))]
-extern crate sapp_dummy as sapp;
+pub extern crate sapp_dummy as sapp;
 #[cfg(target_os = "linux")]
-extern crate sapp_linux as sapp;
+pub extern crate sapp_linux as sapp;
 #[cfg(target_arch = "wasm32")]
-extern crate sapp_wasm as sapp;
+pub extern crate sapp_wasm as sapp;
 #[cfg(windows)]
-extern crate sapp_windows as sapp;
+pub extern crate sapp_windows as sapp;
 #[cfg(target_os = "android")]
-extern crate sapp_android as sapp;
+pub extern crate sapp_android as sapp;
 
 pub mod clipboard;
 pub mod conf;
@@ -32,7 +32,7 @@ pub use graphics::*;
 
 use std::ffi::CString;
 
-pub use sapp::*;
+pub use sapp::{rand, RAND_MAX};
 
 pub mod date {
     #[cfg(not(target_arch = "wasm32"))]
