622adbeeef1ef65716ae3ea40737f4064824a9a9
Nokola <nikolami@outlook.com>
2020-03-28 18:15:02 -0700

    Update Windows support with more GL functions from OpenGL32.lib

--- native/sapp-windows/external/sokol/sokol_app.h
+++ native/sapp-windows/external/sokol/sokol_app.h
@@ -2745,6 +2745,7 @@ _SOKOL_PRIVATE const _sapp_gl_fbconfig* _sapp_gl_choose_fbconfig(const _sapp_gl_
 #include <windowsx.h>
 #include <shellapi.h>
 #pragma comment (lib, "Shell32.lib")
+#pragma comment (lib, "Opengl32.lib")
 
 #if defined(SOKOL_D3D11)
 #ifndef D3D11_NO_HELPERS
