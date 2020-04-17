62dc453dd19ec97f10ced452896ed1a95be4aa5a
Fedor Logachev <not.fl3@gmail.com>
2020-04-16 21:21:26 -0500

    Support arrays in uniforms

--- native/sapp-wasm/js/gl.js
+++ native/sapp-wasm/js/gl.js
@@ -481,49 +481,49 @@ var importObject = {
         glUniform1fv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform1fv', 'location');
             assert((value & 3) == 0, 'Pointer to float data passed to glUniform1fv must be aligned to four bytes!');
-            var view = getArray(value, Float32Array, 1);
+            var view = getArray(value, Float32Array, 1 * count);
             gl.uniform1fv(GL.uniforms[location], view);
         },
         glUniform2fv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform2fv', 'location');
             assert((value & 3) == 0, 'Pointer to float data passed to glUniform2fv must be aligned to four bytes!');
-            var view = getArray(value, Float32Array, 2);
+            var view = getArray(value, Float32Array, 2 * count);
             gl.uniform2fv(GL.uniforms[location], view);
         },
         glUniform3fv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform3fv', 'location');
             assert((value & 3) == 0, 'Pointer to float data passed to glUniform3fv must be aligned to four bytes!');
-            var view = getArray(value, Float32Array, 3);
+            var view = getArray(value, Float32Array, 4 * count);
             gl.uniform3fv(GL.uniforms[location], view);
         },
         glUniform4fv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform4fv', 'location');
             assert((value & 3) == 0, 'Pointer to float data passed to glUniform4fv must be aligned to four bytes!');
-            var view = getArray(value, Float32Array, 4);
+            var view = getArray(value, Float32Array, 4 * count);
             gl.uniform4fv(GL.uniforms[location], view);
         },
         glUniform1iv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform1fv', 'location');
             assert((value & 3) == 0, 'Pointer to i32 data passed to glUniform1iv must be aligned to four bytes!');
-            var view = getArray(value, Int32Array, 1);
+            var view = getArray(value, Int32Array, 1 * count);
             gl.uniform1iv(GL.uniforms[location], view);
         },
         glUniform2iv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform2fv', 'location');
             assert((value & 3) == 0, 'Pointer to i32 data passed to glUniform2iv must be aligned to four bytes!');
-            var view = getArray(value, Int32Array, 2);
+            var view = getArray(value, Int32Array, 2 * count);
             gl.uniform2iv(GL.uniforms[location], view);
         },
         glUniform3iv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform3fv', 'location');
             assert((value & 3) == 0, 'Pointer to i32 data passed to glUniform3iv must be aligned to four bytes!');
-            var view = getArray(value, Int32Array, 3);
+            var view = getArray(value, Int32Array, 3 * count);
             gl.uniform3iv(GL.uniforms[location], view);
         },
         glUniform4iv: function (location, count, value) {
             GL.validateGLObjectID(GL.uniforms, location, 'glUniform4fv', 'location');
             assert((value & 3) == 0, 'Pointer to i32 data passed to glUniform4iv must be aligned to four bytes!');
-            var view = getArray(value, Int32Array, 4);
+            var view = getArray(value, Int32Array, 4 * count);
             gl.uniform4iv(GL.uniforms[location], view);
         },
         glBlendFunc: function (sfactor, dfactor) {
@@ -588,12 +588,6 @@ var importObject = {
         glUseProgram: function (program) {
             GL.validateGLObjectID(GL.programs, program, 'glUseProgram', 'program');
             gl.useProgram(GL.programs[program]);
-        },
-        glUniform4fv: function (location, count, value) {
-            GL.validateGLObjectID(GL.uniforms, location, 'glUniformMatrix4fv', 'location');
-            assert((value & 3) == 0, 'Pointer to float data passed to glUniformMatrix4fv must be aligned to four bytes!');
-            var view = getArray(value, Float32Array, 4);
-            gl.uniform4fv(GL.uniforms[location], view);
         },
         glGenVertexArrays: function (n, arrays) {
             _glGenObject(n, arrays, 'createVertexArray', GL.vaos, 'glGenVertexArrays');
