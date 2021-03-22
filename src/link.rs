4d1769183f085ec09c8e57359add8f471a523793
ilya sheprut <optozorax@gmail.com>
2021-03-22 16:50:38 +0700

    fix compilation for dummy target

--- src/link.rs
+++ src/link.rs
@@ -1,6 +1,7 @@
 //! Functions to open links
 
 /// If not wasm, then open link in browser. If target is wasm, then link can be opened in the same tab, or in a new tab. But when link is opened in a new tab, browser may block it and ask user permission to do it.
+#[allow(unused_variables)]
 pub fn open(url: &str, new_tab: bool) {
     #[cfg(target_arch = "wasm32")]
     {
@@ -13,9 +14,13 @@ pub fn open(url: &str, new_tab: bool) {
         };
     }
 
-    #[cfg(not(target_arch = "wasm32"))]
+    #[cfg(any(
+        all(target_os = "linux", feature = "sapp-linux"),
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "android",
+    ))]
     {
-        drop(new_tab); // used only in wasm
         if let Err(err) = webbrowser::open(url) {
             eprintln!("Failed to open url: {}, url: `{}`", err, url);
         }
