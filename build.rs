098cde1ee7c0024cb1913f24cf481e256bf8f353
Fedor <not.fl3@gmail.com>
2023-03-17 15:03:46 -0600

    ios: fix build.rs

--- build.rs
+++ build.rs
@@ -3,11 +3,7 @@ use std::env;
 fn main() {
     let target = env::var("TARGET").unwrap_or_else(|e| panic!("{}", e));
 
-    if target.contains("darwin") {
-        println!("cargo:rustc-link-lib=framework=OpenGL");
-    }
-
-    if target.contains("ios") {
-        println!("cargo:rustc-link-lib=framework=OpenGLES");
+    if target.contains("darwin") || target.contains("ios"){
+        println!("cargo:rustc-link-lib=framework=MetalKit");
     }
 }
