851b98888f437e769d5e7995d3b4290a885e4fd3
Mario Holubar <34423543+mario-holubar@users.noreply.github.com>
2023-06-15 17:15:35 +0200

    remove unused vertex array

--- src/graphics/gl.rs
+++ src/graphics/gl.rs
@@ -397,10 +397,7 @@ impl GlContext {
                 GL_FRAMEBUFFER_BINDING,
                 &mut default_framebuffer as *mut _ as *mut _,
             );
-            let mut vao = 0;
 
-            glGenVertexArrays(1, &mut vao as *mut _);
-            glBindVertexArray(vao);
             GlContext {
                 default_framebuffer,
                 shaders: vec![],
