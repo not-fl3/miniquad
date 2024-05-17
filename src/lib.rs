838362bb264d805a52a829a7905efecea06813c1
mandroll <157735787+mandroll@users.noreply.github.com>
2024-05-17 16:33:30 -0400

    native/window: {get/set}_window_position

--- src/lib.rs
+++ src/lib.rs
@@ -212,6 +212,19 @@ pub mod window {
         });
     }
 
+    pub fn set_window_position(new_x: u32, new_y: u32) {
+        let mut d = native_display().lock().unwrap();
+        d.native_requests.send(native::Request::SetWindowPosition { new_x, new_y });
+    }
+
+    /// Get the position of the window.
+    /// TODO: implement for other platforms
+    #[cfg(target_os = "windows")]
+    pub fn get_window_position() -> (u32, u32) {
+        let mut d = native_display().lock().unwrap();
+        d.screen_position
+    }
+
     pub fn set_fullscreen(fullscreen: bool) {
         let mut d = native_display().lock().unwrap();
         d.native_requests
