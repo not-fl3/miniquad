52411111b996ca3afe035ad34462f68451e7b605
Wael El Oraiby <wael.eloraiby@gmail.com>
2024-06-21 08:03:20 -0400

    Add reference to libxkbcommon to shell.nix

--- shell.nix
+++ shell.nix
@@ -7,12 +7,13 @@ in pkgs.mkShell {
     libGL
     xorg.libX11
     xorg.libXi
+    libxkbcommon
   ];
 
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
       with pkgs;
-      pkgs.lib.makeLibraryPath [ libGL xorg.libX11 xorg.libXi ]
+      pkgs.lib.makeLibraryPath [ libGL xorg.libX11 xorg.libXi libxkbcommon ]
     }"
   '';
 }
